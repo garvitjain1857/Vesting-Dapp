a6e36fc24eea0ce4414dc8970c25aaa5fbd5aba7
cybranex-ocr <cybranex@britsure.com>
2024-06-19 14:56:21 +0530

    committed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Container, Box, TextField, Button, MenuItem, Typography } from '@mui/ma
 import { ethers } from 'ethers';
 import VestingContractABI from './VestingContractABI.json';
 
-const contractAddress = "0x0bbb10ce367b679e476c6a5cfdabafbe897d2c26";
+const contractAddress = "0x3b7150655733abf7cbdd3500a0a840a9402bcf7f";
 
 const App = () => {
   const [provider, setProvider] = useState(null);
